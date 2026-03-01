9615af0216827e50334cc446f3434e287ccb7fe2
eduardo-pc <e.truchado@gmail.com>
2026-03-01 20:27:31 +0100

    Ej1

---
+++ Fractales/Medida de area por imagen.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+img1 = cv2.imread("shapes.png")
+img = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(img,10,255,0)
+contours,hierarchy = cv2.findContours(thresh, 1, 2)
+print("Number of contours in image:",len(contours))
+
+for i, cnt in enumerate(contours):
+   M = cv2.moments(cnt)
+   if M['m00'] != 0.0:
+      x1 = int(M['m10']/M['m00'])
+      y1 = int(M['m01']/M['m00'])
+   area = cv2.contourArea(cnt)
+   perimeter = cv2.arcLength(cnt, True)
+   perimeter = round(perimeter, 4)
+   print(f'Area of contour {i+1}:', area)
+   print(f'Perimeter of contour {i+1}:', perimeter)
+   img1 = cv2.drawContours(img1, [cnt], -1, (0,255,255), 3)
+   cv2.putText(img1, f'Area :{area}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+   cv2.putText(img1, f'Perimeter :{perimeter}', (x1, y1+20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+cv2.imshow("Image", img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Qiskit/Qiskit_pruebas.py
+++ Qiskit/Qiskit_pruebas.py
@@ -1,29 +1,21 @@
 from qiskit import QuantumCircuit
-from qiskit._accelerate import results
 from qiskit_aer import AerSimulator
-import numpy as np
-
-pi = np.pi
-
-
-theta = [0, pi/3, pi/2, pi]
-shots = 100
-
-# Creamos un simulador cu´antico
-sim = AerSimulator()
-# Creamos un circuito con 1 qubit y 1 bit cl´asico
-qc = QuantumCircuit(1, 1)
-# Aplicamos una rotaci´on Ry(theta) al qubit
-qc.ry(theta[1], 0)
-# Medimos el qubit en la base computacional (Z)
-qc.measure(0, 0)
-# Ejecutamos el circuito un n´umero finito de veces (shots)
-job = sim.run(qc, shots=shots)
-# Obtenemos el resultado de la simulaci´on
-result = job.result()
-# Extraemos los resultados de la medida
-counts = result.get_counts(qc)
-
-print(results)
+from qiskit import transpile
+import matplotlib.pyplot as plt
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
+# Inicializar el simulador
+simulator = AerSimulator()
+# Transpilar el circuito
+transpiled_qc = transpile(qc, simulator)
+# Ejecutar el circuito
+result = simulator.run(transpiled_qc, shots=1024).result()
+# Obtener los resultados
+counts = result.get_counts()
 print(counts)
-qc.draw()
+# Dibujar el circuito
+qc.draw(output='mpl')
+# Mostrar la gráfica del circuito
+plt.show()
