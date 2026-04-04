e6a82f253234c93e86eed42d61a17b9302235130
jukefr14 <jsrubio2005@gmail.com>
2026-04-04 22:47:51 +0200

    Difracción terminada a falta de cafe benito

---
+++ Difraccion/practica/Datos_cafe_Benito.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#Tablas de la forma [D (distancia rendija-caja), d(m=1 m=-1) (distancia entre m=-1 y m=1)]
+#D en cm X_min mm
+red_distanciaG = np.array([[90, 9.4],
+							[85, 8.11],
+							[80, 5.03],
+							[75, 5.64],
+							[70, 4.43]])
+
+red_distanciaP = np.array([[90, 3.62],
+							[85, 3.71],
+							[80, 2.01],
+							[75, 2.12],
+							[70, 2.81]])
+
+red_radio = np.array([[90, 40.32],
+						[85, 37.83],
+						[80, 34.92],
+						[75, 31.92],
+						[70, 30.91]])
+
+distancias = np.transpose(red_distanciaG)[0]
+
+d_g = np.transpose(red_distanciaG)[1]
+d_p = np.transpose(red_distanciaP)[1]
+d_r = np.transpose(red_radio)[1]
+
+indices_tabla = ["D (cm)", r"$\Delta D$ (cm)"]
